1ab9f76ff000cfd274ab24d9ec15d0d883f5e9a5
Franz Asperger <f.asperger@gmail.com>
2022-08-23 14:29:45 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+
+
+
+app.get("/", function(req, res){
+    
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=mexico&units=metric&appid=22e47705da4bbf6ad45eb6a64e67ca5d";
+    https.get(url, function(response){
+
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const city = weatherData.name;
+            const weatherIcon = weatherData.weather[0].icon;
+            const weatherImg = "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
+            res.write("<p>The weather is currently " + description + "</p>");
+            res.write("<h1>The temperature in " + city + " is " + temp + " degrees Celsius.</h1>");
+            res.write("<img src="+weatherImg+">");
+            res.send();
+        });
+    });
+});
+
+
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+});
